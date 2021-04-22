c19b30a79db99279e3ab120fea957c402ba5ab3f
Joco321 <32468492+Joco321@users.noreply.github.com>
2021-04-21 18:24:12 -0700

    working
    lots to change but this is the working code from the youtuber

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var redirect_uri = "https://makeratplay.github.io/SpotifyWebAPI/"; // change this your value
-//var redirect_uri = "http://127.0.0.1:5500/index.html";
+var redirect_uri = "http://127.0.0.1:5500/index.html"; // change this your value
+// var redirect_uri = "http://127.0.0.1:5500/index.html";
  
 
 var client_id = ""; 
